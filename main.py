d71682028daf1da2b4526b56c425c60e7a6f3c1f
pww217 <pwilson@anaconda.com>
2023-09-19 19:44:56 -0500

    write some functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os, logging, json
 from linkedin_api import Linkedin
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -8,46 +9,60 @@ sh = logging.StreamHandler()
 sh.setFormatter(formatter)
 logger.addHandler(sh)
 
-api_logger = logging.getLogger('linkedin_api')
+api_logger = logging.getLogger("linkedin_api")
 api_logger.setLevel(logging.DEBUG)
 
+
+def get_convo_urns(convos):
+    all_urns = []
+    for c in convos:
+        urn = c["entityUrn"].split(":")[-1]
+        all_urns.append(urn)
+    return all_urns
+
+
+def check_recruiter_status(convos):
+    events = convos["elements"]["events"]
+    rstatus = events[0]["eventContent"][
+        "com.linkedin.voyager.messaging.event.MessageEvent"
+    ]["customContent"]["com.linkedin.voyager.messaging.event.message.InmailContent"][
+        "recruiterInmail"
+    ]
+    return rstatus
+
+
+def check_unreads(inbox):
+    unreads = []
+    for message in inbox["elements"]:
+        is_read = message["read"]
+        if is_read == False:
+            unreads.append(message)
+    with open("unreads.txt", "w") as f:
+        f.write(json.dumps(unreads, indent=2))
+    return unreads
+
+
 def main():
+    # user = os.getenv('LKDIN_USER')
+    # pw = os.getenv('LKDIN_PW')
 
-  user = os.getenv('LKDIN_USER')
-  pw = os.getenv('LKDIN_PW')
+    # # Ideally would improve on the env var method
+    # api = Linkedin(user, pw)
 
-  # Authenticate using any Linkedin account credentials
-  api = Linkedin(user, pw)
+    # inbox = api.get_conversations()
+    # with open('inbox.txt', 'w') as f:
+    #  f.write(json.dumps(inbox, indent=2))
 
-  convos = json.dumps(api.get_conversations(), indent=2)
-  with open('convos.txt', 'w') as f:
-    f.write(convos)
+    with open("inbox.txt", "r") as f:
+        inbox = json.loads(f.read())
 
-  one_convo = json.dumps(api.get_conversation(conversation_urn_id="2-M2QwZTU2OTUtYWE0OS00ODFkLWExMDktY2YxMDU1MDU1OTEyXzAxMg=="), indent=2)
-  with open('one_convo.txt', 'w') as f:
-    f.write(one_convo)
+    unreads = check_unreads(inbox)
+    pprint(f"Unread messages: {len(unreads)}")
 
-  profile = json.dumps(api.get_profile(public_id="irina-chernous-905483268", urn_id=None), indent=2)
-  with open('profile.txt', 'w') as f:
-    f.write(profile)
-
-  chat = json.dumps(api.get_conversation_details("ACoAAEGQ0VUB1QS0cJ-Xt-Us4VA8U7vuWjJ11Vc"), indent=2)
-  with open('chat.txt', 'w') as f:
-    f.write(chat)
-
-if __name__ == '__main__':
-  main()
+    # convos = api.get_conversations()
+    # with open('convos.txt', 'w') as f:
+    #   f.write(json.dumps(convos, indent=2))
 
 
-  """
-  ...
-    "metadata": {
-    "unreadCount": 0
-  }
-
-  "elements": [
-    {
-      "read": true,
-      "totalEventCount": 4
-    }
-  """
+if __name__ == "__main__":
+    main()
