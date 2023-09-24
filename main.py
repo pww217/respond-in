6f1e6ed1cb13698e302326b058ff60773084a6c1
pww217 <pwilson@anaconda.com>
2023-09-24 12:05:24 -0500

    basic 1.0 functionality

--- main.py
+++ main.py
@@ -34,13 +34,12 @@ def get_recruiter_message(message):
     event = message["events"][0]
     content = event["eventContent"]["com.linkedin.voyager.messaging.event.MessageEvent"]
     is_recruiter = filter_for_recruiters(content)
-    
+
     if is_recruiter:
         map = {"urn": urn, "content": content}
         return map
     else:
-        map = {"urn": urn, "content": None}
-        return map
+        return None
 
 
 def filter_for_recruiters(content):
@@ -61,34 +60,35 @@ def filter_for_recruiters(content):
 def respond_and_mark_read(api, template, message):
     urn = message["urn"]
     content = message["content"]
-    #api.send_message(template, conversation_urn_id=urn)
-    #api.mark_conversation_as_seen(urn)
+    api.send_message(template, conversation_urn_id=urn)
+    api.mark_conversation_as_seen(urn)
+    logger.info(f"Finished transaction for {urn}")
 
 
-def parse_message(message): 
+def parse_message(message):
     subject = message.get("subject")
     text = message["attributedBody"]["text"].strip("\n")
     print(f"Subject: {subject}\n\n{text}\n-----------------------------------")
 
 
 def main():
-    user = os.getenv('LKDIN_USER')
-    pw = os.getenv('LKDIN_PW')
+    user = os.getenv("LKDIN_USER")
+    pw = os.getenv("LKDIN_PW")
 
     # Ideally would improve on the env var method
     api = Linkedin(user, pw)
 
-    # inbox = api.get_conversations()
-    # with open('inbox.txt', 'w') as f:
-    #  f.write(json.dumps(inbox, indent=2))
+    inbox = api.get_conversations()["elements"]
+    with open("inbox.txt", "w") as f:
+        f.write(json.dumps(inbox, indent=2))
 
-    with open("inbox.txt", "r") as f:
-        inbox = json.loads(f.read())["elements"]
+    # with open("inbox.txt", "r") as f:
+    #     inbox = json.loads(f.read())["elements"]
 
     unread = get_unread(inbox)
 
-    with open("unread.txt", "w") as f:
-        f.write(json.dumps(unread, indent=2))
+    # with open("unread.txt", "w") as f:
+    #     f.write(json.dumps(unread, indent=2))
 
     recruiter_messages = []
     for item in unread:
@@ -97,17 +97,17 @@ def main():
             pass
         else:
             recruiter_messages.append(message)
-    
+
+    pprint(f"Messages needing response: {len(recruiter_messages)}")
+
     with open("recruiter-messages.txt", "w") as f:
         f.write(json.dumps(recruiter_messages, indent=2))
 
     with open("template.txt") as f:
         template = f.read()
-    print(template)
     for item in recruiter_messages:
         respond_and_mark_read(api, template, item)
 
-    pprint(f"Messages needing response: {len(unread)}")
 
 if __name__ == "__main__":
     main()
